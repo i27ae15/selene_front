8c7a9ec32386d245630672e686a644562bfab640
i27ae15 <andresruse18@gmail.com>
2022-09-27 15:24:26 -0400

    add response, delete node, js bug, again

--- src/components/selene-node/SeleneNode.component.jsx
+++ src/components/selene-node/SeleneNode.component.jsx
@@ -17,6 +17,56 @@ function SeleneNode({nodeIndex, availableNodes, node, functions}) {
     const [doBefore, setDoBefore] = useState([]);
     const [doAfter, setDoAfter] = useState([]);
 
+    const [responses, setResponses] = useState([{}]);
+
+    /*
+        The response object has the following structure:
+
+        "responses": [
+            {
+                "type": "input",
+                "properties": {
+                    "input_type": "options",
+                    "input_name": "doctor",
+                    "options": ["less thaan 6 months", "more than 6 months"]
+                },
+                "text": "Select you desire amount of time to rent"
+            }
+        ]
+
+    */
+
+    const addNewResponse = () => {
+
+        if (!responses[responses.length - 1]) {
+            return;
+        }
+
+        setResponses([...responses, false]);
+    }
+
+    const deleteResponse = (responseIndex) => {
+        let filter = responses.filter((object, index) => {
+            
+            if (index !== Number(responseIndex)) {
+                return true;
+            }
+            return false;
+            
+        });
+
+        setResponses(filter);
+    }
+
+
+    const updateResponse = (responseIndex, property, value) => {
+        let newResponses = [...responses];
+        newResponses[responseIndex][property] = value;
+        setResponses(newResponses);
+
+        functions.updateProperty(nodeIndex, 'responses', newResponses);
+    }
+
 
     const addNewNodeOnOption = (node, option) => {
             setNextNodeOnOption([...nextNodeOnOption, {
@@ -50,7 +100,6 @@ function SeleneNode({nodeIndex, availableNodes, node, functions}) {
             node: option,
         }]);
     }
-
 
 
     return (
@@ -106,11 +155,48 @@ function SeleneNode({nodeIndex, availableNodes, node, functions}) {
                         {/* ----------------- Responses section -----------------  */}
                         <div className="mb-4">
                             <h3>Responses</h3>
+
                             <div id='responses'>
+                                {
+                                    responses.map((response, responseIndex) => {
+                                        return (
+                                            <div className='row' key={responseIndex}>
+                                                <div className='col-lg-3 col-md-3 col-3'>
+                                                    <div class="form-group">
+                                                        <label for="sel1">Select response type:</label>
+                                                        <select class="form-control" id="sel1" onChange={(e) => {
+                                                            updateResponse(responseIndex, 'type', e.target.value);
+                                                        }}>
+                                                            <option value='input'>Input</option>
+                                                            <option value='text'>Text</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div className='col-lg-8 col-md-8 col-8'>
+                                                    <div class="form-group">
+                                                        <label for="sel1">Response text:</label>
+                                                        <input type="text" className="form-control" value={response.text} onChange={(e) => {
+                                                            updateResponse(responseIndex, 'text', e.target.value);
+                                                        }}/>
+                                                    </div>
+                                                </div>
+                                                <div className='col-lg-1 col-md-1 col-1'>
+                                                    <div class="form-group">
+                                                        <label for="sel1">Delete</label>
+                                                        <button className='btn btn-danger' onClick={(e) => {
+                                                            deleteResponse(e.target.id);
+                                                        }} id={responseIndex}>Delete</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+
+                                }
 
                             </div>
 
-                            <button className='btn btn-primary'>Add new response</button>
+                            <button className='btn btn-primary' onClick={addNewResponse}>Add new response</button>
                             
                         </div>
                         {/* ----------------------------------------------------- */}
